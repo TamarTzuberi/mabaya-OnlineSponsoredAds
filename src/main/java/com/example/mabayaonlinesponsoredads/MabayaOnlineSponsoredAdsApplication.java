a1a00d4fa4e405bbafd02ab447dd49fa8574e6aa
TamarTzuberi <tamar.tzuberi1@gmail.com>
2023-08-12 21:02:02 +0300

    Creating new spring boot project

---
+++ src/main/java/com/example/mabayaonlinesponsoredads/MabayaOnlineSponsoredAdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mabayaonlinesponsoredads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabayaOnlineSponsoredAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabayaOnlineSponsoredAdsApplication.class, args);
+	}
+
+}
